84c35c17aedc3e98ebdd87f4580a89e3038d593d
Your Name <you@example.com>
2023-07-20 18:40:36 +0545

    dynamic render from file(optioons.ts)

--- App.tsx
+++ App.tsx
@@ -30,24 +30,24 @@
 
 import React,{useState} from "react";
 import { View, Text, Image , StyleSheet, Pressable} from "react-native";
-
+import  question from './assets/images/data/options'
 
 
 
 const ImageOption = (props:any)=>{
-  const [pres, setPres] = useState(false);
+  // const [pres, setPres] = useState(false);
 
-  if(pres){
+  if(props.isSelected){
     console.warn(props.id);
   }
 
   // const {name, imaged} = props;
   return ( <Pressable 
-            onPress={()=>setPres(true)} 
+            onPress={()=>props.handleOption()} 
             // style={styles.box}
-            style={[pres ? styles.activeBox : styles.box]}
+            style={[props.isSelected ? styles.activeBox : styles.box]}
             >
-              <Image source={props.imaged} style={styles.images} />  
+              <Image source={{uri:props.imaged}} style={styles.images} />  
               <Text style={{fontSize:20, marginTop:10, color:"red"}}>{props.name}</Text>
           </Pressable>
         );
@@ -55,16 +55,26 @@ const ImageOption = (props:any)=>{
 
 const App = () => {
 
+  const [selectedOption,setSelectedOption] = useState<any>(null)
+
   return (
     <View style={{display:"flex", flex:1}}>
       <View style={{backgroundColor:"skyblue"}}>
         <Text style={styles.text}>Example on Flexbox.</Text>
       </View>
       <View style={styles.boxWrapper}>
-       <ImageOption name={"lorem"}  imaged={require("./assets/images/1.jpg")} id={1}/>
-       <ImageOption name={"lorem2"} imaged={require("./assets/images/2.jpg")} id={2}/>
-       <ImageOption name={"lorem3"} imaged={require("./assets/images/3.jpg")} id={3}/>
-       <ImageOption name={"lorem4"} imaged={require("./assets/images/4.jpg")} id={4}/>
+     {
+      question.options.map((option)=>{
+        return        <ImageOption
+        isSelected={selectedOption?.id==option.id}
+         name={option.text} imaged={option.image} id={option.id} handleOption={()=>{
+          setSelectedOption(option)
+        }}/>
+      })
+     }
+       {/* <ImageOption name={"lorem2"} imaged={require("./assets/images/2.jpg")} id={2}/> */}
+       {/* <ImageOption name={"lorem3"} imaged={require("./assets/images/3.jpg")} id={3}/>
+       <ImageOption name={"lorem4"} imaged={require("./assets/images/4.jpg")} id={4}/> */}
       
         {/* <View style={styles.box}>
           <Image source={require("./assets/images/2.jpg")} style={styles.images} />  
---
+++ assets/images/data/options.ts
@@ -0,0 +1,31 @@
+export default {
+    question: 'Which of these is "the glass"?',
+    options: [
+      {
+        id: 'option1',
+        text:"cup",
+        image:
+          'https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png',
+      },
+      {
+        id: 'option2',
+        text:"glass",
+        image:
+          'https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png',
+        correct: true,
+      },
+      {
+        id: 'option3',
+        text: 'milk',
+        image:
+          'https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/milk.png',
+
+      },
+      {
+        id: 'option4',
+        text: 'coffee',
+        image:
+          'https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/coffee.png',
+      },
+    ],
+  };
