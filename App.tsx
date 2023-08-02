05f39f7257f2ee42311c99344105dcf39282d299
Your Name <you@example.com>
2023-08-02 21:20:44 +0545

    Progress Bar and LifeLine

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { View, Text, Image, StyleSheet, Pressable, Alert, Modal } from "react-native";
 import question from './assets/images/data/options'
 import questions from './assets/images/data/imageMultipleChoice'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import iconn from './assets/images/data/icons'
 
 
 const ImageOption = (props: any) => {
@@ -23,17 +25,82 @@ const ImageOption = (props: any) => {
   );
 }
 
+
+const IconComp = (props: any) => {
+
+  return (
+    <Ionicons name={props.iname} size={props.isize} color={props.heartid < props.id ? "red" : "gray"} />
+  )
+}
+
+
+
+const ProgressBar = ({ progress }: any) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        // display:"flex",
+        height: 30,
+        backgroundColor: '#ecdceb',
+        borderRadius: 40,
+        margin: 10,
+        // alignContent:"center"
+      }}>
+      <View
+        style={{
+          backgroundColor: 'orange',
+          height: 10,
+          width: `${progress * 100}%`,
+          borderRadius: 40,
+        }}
+      />
+    </View>
+  );
+};
+
+
+
 const App = () => {
   //HOOK UseState
   const [selectedOption, setSelectedOption] = useState<any>(null)
   const [questionIndex, setCurrentIndex] = useState(0)
+  const [heartId, setHeartId] = useState(0)
+  const [tracker, setTracker] = useState(0)
   const [isModal, setIsModal] = useState(false)
+  const [isOverModal, setIsOverModal] = useState(false)
+
 
   return (
+    // Main Container
     <View style={{ display: "flex", flex: 1 }}>
+
+      <View style={{ flexDirection: 'row', height: 30 }}>
+        <View style={{ flex: 3 }}>
+          <ProgressBar progress={tracker / questions.length} />
+        </View>
+        <View style={{ flex: 2 }} >
+          <View style={{ flexDirection: "row", alignContent: "center" }}>
+
+            {iconn.map((prop) => {
+              return (
+                <IconComp iname={prop.name} isize={prop.size} icolor={prop.color} heartid={heartId} id={prop.id} />
+              );
+            })
+            }
+
+            {/* <Ionicons name="heart" size={20} color="red" /> */}
+
+          </View>
+        </View>
+      </View>
+
+      {/* Question Container */}
       <View style={{ backgroundColor: "skyblue" }}>
         <Text style={styles.text}>{questions[questionIndex].question}</Text>
       </View>
+
+      {/* Options Container */}
       <View style={styles.boxWrapper}>
         {
           questions[questionIndex].options.map((option) => {
@@ -65,23 +132,23 @@ const App = () => {
           <Text style={{fontSize:20, marginTop:10, color:"red"}}>lorem</Text>
         </View> */}
 
+        {/*Modal End Display */}
         <Modal visible={isModal}
           animationType="slide"
           transparent={true}
-        // presentationStyle="pageSheet"
         >
-
           <View style={styles.centeredView}>
-            <View style={{justifyContent:"center", alignItems:"center"}}>
-              <View style={{ display: "flex", flexDirection: "row", backgroundColor: "#485696", borderRadius: 10, justifyContent: "center", width:"95%", gap:10}}>
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+
+              <View style={{ display: "flex", flexDirection: "row", backgroundColor: "#485696", borderRadius: 10, justifyContent: "center", width: "95%", gap: 10 }}>
                 <Pressable
                   onPress={() => {
                     setCurrentIndex(0);
+                    setTracker(0);
+                    setHeartId(0);
                     setSelectedOption(null);
                     setIsModal(false);
-                    // Alert.alert('Game over');
                   }}
-
                   style={{ display: "flex", justifyContent: "center" }}
                 >
                   <Image source={require("./assets/images/close.png")} style={styles.imageEnd} />
@@ -90,11 +157,11 @@ const App = () => {
                 <Pressable
                   onPress={() => {
                     setCurrentIndex(0);
+                    setTracker(0);
+                    setHeartId(0);
                     setSelectedOption(null);
                     setIsModal(false);
-                    // Alert.alert('Game over');
                   }}
-
                   style={{ display: "flex", justifyContent: "center" }}
                 >
                   <Image source={require("./assets/images/reload.jpg")} style={styles.imageEnd} />
@@ -105,14 +172,73 @@ const App = () => {
           </View>
         </Modal>
 
+
+        {/*Modal GameOver */}
+        <Modal visible={isOverModal}
+          animationType="slide"
+          transparent={true}
+        >
+          <View style={styles.centeredView}>
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+
+              <View style={{ display: "flex", flexDirection: "row", backgroundColor: "#485696", borderRadius: 10, justifyContent: "center", width: "95%", gap: 10 }}>
+                <Pressable
+                  onPress={() => {
+                    setCurrentIndex(0);
+                    setTracker(0);
+                    setSelectedOption(null);
+                    setHeartId(0);
+                    setIsOverModal(false);
+                  }}
+                  style={{ display: "flex", justifyContent: "center" }}
+                >
+                  <Image source={require("./assets/images/reload.jpg")} style={styles.imageEnd} />
+                  <Text style={{color:"white", fontSize:28, textAlign:"center"}}>GameOver</Text>
+                </Pressable>
+              </View>
+
+            </View>
+          </View>
+        </Modal>
+
+        {/* Check Button */}
         <Pressable
           onPress={() => {
-            const tracker = questionIndex + 1;
-            if (tracker >= questions.length) {
-              setIsModal(true);
+
+            if (tracker === questions.length - 1) {
+              if (selectedOption['correct']) {
+                setTracker(tracker + 1);
+                setSelectedOption(null);
+                setIsModal(true);
+              } else {
+                Alert.alert('Wrong answer');
+                setHeartId(heartId + 1);
+                if (heartId == 4) {
+                  setIsOverModal(true);
+                  setSelectedOption(null);
+                }
+              }
             } else {
-              setCurrentIndex(tracker);
+              // setCurrentIndex(tracker);
               setSelectedOption(null);
+              if (selectedOption['correct']) {
+                if (tracker < questions.length - 1) {
+                  setCurrentIndex(questionIndex + 1);
+                  setTracker(tracker + 1);
+                  // Alert.alert(`${tracker}`);
+                }
+
+              } else {
+                // decrease life if user choses wrong answer
+                // check if life ==0 then display Alert with message game over
+                // Restart game
+                setHeartId(heartId + 1);
+                Alert.alert(`Wrong answer`);
+                if (heartId == 4) {
+                  setIsOverModal(true);
+                  setSelectedOption(null);
+                }
+              }
             }
           }}
           disabled={selectedOption == null}
@@ -121,15 +247,14 @@ const App = () => {
             padding: 15, marginBottom: 10, borderRadius: 10, width: "40%"
           }}
         >
-          <Text style={{ fontSize: 20, textAlign: "center", color: "white" }}>Check</Text>
+          <Text style={{ fontSize: 20, textAlign: "center", color: "white" }}>Check
+            {/* <Ionicons size={30} color="white" name="heart" /> */}
+          </Text>
         </Pressable>
 
       </View>
-
     </View>
-
   );
-
 };
 
 export default App;
@@ -152,7 +277,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "grey",
     borderRadius: 15,
-    margin: 10,
+    margin: 7,
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
@@ -183,7 +308,7 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: "grey",
     borderRadius: 15,
-    margin: 10,
+    margin: 7,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
@@ -203,14 +328,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   imageEnd: {
-    margin:10,
+    margin: 10,
     padding: 15,
     width: 150,
     height: "50%",
     borderRadius: 25,
     resizeMode: "contain",
-    borderWidth: 2,
-    borderColor: "red"
+    borderWidth: 4,
+    borderColor: "gray"
   }
 
 });
---
+++ assets/images/data/icons.ts
@@ -0,0 +1,46 @@
+export default [
+    {
+        id:1,
+        name: "heart",
+        size: 30,
+        color: "red"
+    },
+    {
+        id:2,
+        name: "heart",
+        size: 30,
+        color: "red"
+    },
+    {
+        id:3,
+        name: "heart",
+        size: 30,
+        color: "red"
+    },
+    {
+        id:4,
+        name: "heart",
+        size: 30,
+        color: "red"
+    },    
+    {
+        id:5,
+        name: "heart",
+        size: 30,
+        color: "red"
+    },
+    // {
+    //     ico:[{
+    //         name:"heart",
+    //         size:30,
+    //         color:"red"
+    //     }]
+    // },
+    // {
+    //     ico:[{
+    //         name:"heart",
+    //         size:30,
+    //         color:"red"
+    //     }]
+    // },
+]
